8ab91330d67b3a833843c29f61b09ae0ec993f37
Nathan Nichols <nwnichols02@gmail.com>
2022-07-26 11:21:53 -0600

    converting to tsc

---
+++ dist/index.js
@@ -0,0 +1,30 @@
+"use strict";
+const votingAge = 18;
+if (votingAge >= 18) {
+    console.log('you can vote');
+}
+else {
+    console.log('you cannot vote');
+}
+function multiply() {
+}
+function dogYears() {
+}
+function hungryDog() {
+}
+function miles() {
+}
+function feet() {
+}
+function annoyingSong() {
+}
+function grade() {
+}
+function vowelCounter() {
+}
+function foo() {
+    console.log('its working');
+    return 'bar';
+}
+foo();
+//# sourceMappingURL=index.js.map
